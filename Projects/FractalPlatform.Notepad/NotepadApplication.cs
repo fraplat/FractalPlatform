1f132678a9a7e63bf6245e21000064ad5ccbefc8
Bazist <learn.fractal@gmail.com>
2025-05-03 11:03:55 +0300

    fix Notepad app

--- Projects/FractalPlatform.Notepad/NotepadApplication.cs
+++ Projects/FractalPlatform.Notepad/NotepadApplication.cs
@@ -6,6 +6,6 @@ namespace FractalPlatform.Notepad
     public class NotepadApplication : BaseApplication
     {
         public override void OnStart() =>
-            UsePasword("777", _ => ModifyFirstDocOf("Notes").OpenForm(result => SaveForm()));
+            UsePasword("mypass", _ => ModifyFirstDocOf("Notes").OpenForm(result => SaveForm()));
     }
 }
