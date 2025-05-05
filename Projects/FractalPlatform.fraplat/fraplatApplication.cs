9b43ea315bed5c51526ab9246fdd5fe3c10b3bff
Bazist <learn.fractal@gmail.com>
2025-05-05 16:10:44 +0300

    add some pics

--- Projects/FractalPlatform.fraplat/fraplatApplication.cs
+++ Projects/FractalPlatform.fraplat/fraplatApplication.cs
@@ -4,27 +4,24 @@ using FractalPlatform.Client.UI.DOM;
 using FractalPlatform.Client.App;
 using FractalPlatform.Client.UI;
 
-namespace FractalPlatform.fraplat
-{
-    public class fraplatApplication : BaseApplication
-    {
-        public override void OnStart()
-        {
-            if(!Context.HasUrlTag || Context.UrlTag == "home")
-                    FirstDocOf("Home").OpenForm();
-                else if(Context.UrlTag == "entry") 
-                    FirstDocOf("Entry").OpenForm();
-                else if(Context.UrlTag == "basic") 
-                    FirstDocOf("Basic").OpenForm();
-                else if(Context.UrlTag == "advanced") 
-                    FirstDocOf("Advanced").OpenForm();
-                else if(Context.UrlTag == "enterprise") 
-                    FirstDocOf("Enterprise").OpenForm();
-                else if(Context.UrlTag == "pricing") 
-                    FirstDocOf("Pricing").OpenForm();
-                else if(Context.UrlTag == "about") 
-                    FirstDocOf("About").OpenForm();
-                else NotImplementedMessageBox(); 
+namespace FractalPlatform.fraplat {
+    public class fraplatApplication: BaseApplication {
+        public override void OnStart() {
+            if (!Context.HasUrlTag || Context.UrlTag == "home")
+                FirstDocOf("Home").OpenForm();
+            else if (Context.UrlTag == "entry")
+                FirstDocOf("Entry").OpenForm();
+            else if (Context.UrlTag == "basic")
+                FirstDocOf("Basic").OpenForm();
+            else if (Context.UrlTag == "advanced")
+                FirstDocOf("Advanced").OpenForm();
+            else if (Context.UrlTag == "enterprise")
+                FirstDocOf("Enterprise").OpenForm();
+            else if (Context.UrlTag == "pricing")
+                FirstDocOf("Pricing").OpenForm();
+            else if (Context.UrlTag == "about")
+                FirstDocOf("About").OpenForm();
+            else NotImplementedMessageBox();
         }
     }
 }
