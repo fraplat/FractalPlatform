57bc4fd1b5eb75bd16f3acabd5e6a3974c5b4847
Bazist <learn.fractal@gmail.com>
2025-11-28 23:17:18 +0200

    fix project

--- Projects/FractalPlatform.Teplo/TeploApplication.cs
+++ Projects/FractalPlatform.Teplo/TeploApplication.cs
@@ -1,10 +1,6 @@
-using FractalPlatform.Common.Clients;
-using FractalPlatform.Common.Enums;
 using System.Collections.Generic;
 using System.Linq;
-using FractalPlatform.Database.Engine.Info;
 using FractalPlatform.Database.Engine;
-using FractalPlatform.Client.UI.DOM;
 using FractalPlatform.Client.App;
 using FractalPlatform.Client.UI;
 using System;
